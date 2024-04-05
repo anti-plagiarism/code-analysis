9f1b79c529e66fca59830951dc5cff8ba397cf6a
llav3ji2019 <pavelemelyanov03@mail.ru>
2024-04-05 23:33:51 +0300

    init spring repository

---
+++ src/main/java/com/vk/codeanalysis/CodeAnalysisApplication.java
@@ -0,0 +1,24 @@
+package com.vk.codeanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration;
+import org.springframework.boot.autoconfigure.netty.NettyAutoConfiguration;
+import org.springframework.boot.autoconfigure.web.servlet.MultipartAutoConfiguration;
+import org.springframework.boot.autoconfigure.websocket.servlet.WebSocketServletAutoConfiguration;
+
+@SpringBootApplication(
+        exclude = {
+                GsonAutoConfiguration.class,
+                MultipartAutoConfiguration.class,
+                WebSocketServletAutoConfiguration.class,
+                NettyAutoConfiguration.class,
+        }
+)
+public class CodeAnalysisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeAnalysisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vk/codeanalysis/CodeAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vk.codeanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodeAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
