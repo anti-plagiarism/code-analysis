45de081faf2f8fa46f7c1b3760e31c0e05e495c0
Andrey <70713475+Queenore@users.noreply.github.com>
2024-04-24 18:43:28 +0300

    minor fixes

--- src/main/java/com/vk/codeanalysis/plagiarismalg/Language.java
+++ src/main/java/com/vk/codeanalysis/plagiarismalg/Language.java
@@ -1,5 +1,24 @@
 package com.vk.codeanalysis.plagiarismalg;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public enum Language {
-    JAVA, CPP, PYTHON
+    JAVA, CPP, PYTHON;
+
+    private static final Map<String, Language> STRING_TO_ENUM = new HashMap<>();
+
+    static {
+        for (Language lang : Language.values()) {
+            STRING_TO_ENUM.put(lang.toString().toLowerCase(), lang);
+        }
+    }
+
+    public static Language fromString(String value) {
+        return STRING_TO_ENUM.get(value.toLowerCase());
+    }
+
+    public static String toString(Language lang) {
+        return lang.toString().toLowerCase();
+    }
 }
